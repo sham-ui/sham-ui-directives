b977b5b40cacf4dc4797969da9bfd1c189a4f73c
Eugeny Burnashov <shamcode@gmail.com>
2019-10-24 17:31:20 +0700

    Add doc

--- src/disabled.js
+++ src/disabled.js
@@ -1,4 +1,29 @@
-export default class {
+/**
+ * Directive for "disabled" attribute
+ * @example
+ * <template>
+ *   <input :disabled={{inputDisabled}}/>
+ * </template>
+ * <script>
+ *     import { disabled } from 'sham-ui-directives';
+ *     import { options } from 'sham-ui-macro/babel.macro';
+ *
+ *     class Input extends Template {
+ *         \@options inputDisabled = true;
+ *
+ *         constructor() {
+ *             super( ...arguments );
+ *             this.directives = {
+ *                 ...( this.directives || {} ),
+ *                 disabled
+ *             };
+ *         }
+ *     }
+ *
+ *     export default Input;
+ * </script>
+ */
+export default class disabled {
     constructor() {
         this.node = null;
     }
