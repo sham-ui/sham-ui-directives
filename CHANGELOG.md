efb2ce266eb269f778f28e469f0ac53adac4142f
Eugeny Burnashov <shamcode@gmail.com>
2021-05-14 21:39:04 +0700

    * Update dependencies * Change project structure to sham-ui 5.x

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 5.0.0-alpha.2
+* Update dependencies
+* Change project structure to sham-ui 5.x
+
 # 5.0.0-alpha.1
 * Update dependencies
 * Fix HMR in demo-app
--- demo-app/src/main.js
+++ demo-app/src/main.js
@@ -1,27 +1,9 @@
 import { createDI, start } from 'sham-ui';
+import './styles/main.scss';
 import mainInitializer from './initializers/main';
-
-if ( module.hot ) {
-    if ( window.DI ) {
-        const store = window.DI.resolve( 'sham-ui:store' );
-        const app = store.byId.get( 'app' );
-        if ( undefined !== app ) {
-            app.remove();
-            Array.from( store.byId.values() ).forEach( component => {
-                try {
-                    component.remove();
-                } catch ( e ) {
-                    // eslint-disable-next-line no-empty
-                }
-            } );
-        }
-    }
-    module.hot.accept();
-}
 
 const DI = createDI();
 
-window.DI = DI;
-
 mainInitializer( DI );
 start( DI );
+
